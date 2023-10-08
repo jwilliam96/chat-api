412f701d4824a8975ecb8380f86848089c8eeb88
william sanchez <williambe2021@gmail.com>
2023-10-08 09:33:14 -0500

    dialect

--- src/config/config.js
+++ src/config/config.js
@@ -21,7 +21,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: proces.env.DB_PORT,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
     dialectOptions: { ssl: { required: true, rejectUnauthorized: false } },
